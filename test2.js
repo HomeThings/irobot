c243da5f2e2671a662858479271f432183af6cff
Marshall Rose <mrose17@gmail.com>
2014-01-01 02:10:26 -0800

    initial support for RooWifi
    0. minimize changes to upstream repo
    1. add command sets to encapsulate SCI, OIC500 and current OIC commands
    2. when creating a Robot(), allow user to specify command set, and
    whether to use pre-existing network socket rather than open a serial
    port
    3. invoke commands with the user-specified command set (defaults to the
    current OIC command set).
    4. allow user to specify a low-level send command (necessary for
    RooWifi, unnecessary for other WiFi solutions)
    5. fix but in demo() -- it is being passed the object, not the demo ID
    6. add .gitignore to ignore node_modules
    7. add roowifi.js and test2.js

---
+++ test2.js
@@ -0,0 +1,34 @@
+// similar to test.js, but connects to this IP address
+
+var irobot  = require('./index')
+  , demos   = require('./lib/demos')
+  , roowifi = require('./lib/roowifi')
+  , ipaddr  = '192.168.1.63'
+  ;
+
+roowifi.RooWifi({ ipaddr: ipaddr }, function(err, options) {
+  if (!!err) return console.log(err);
+
+  var robot = new irobot.Robot('tcp', options);
+
+  robot.on('ready', function () {
+    console.log('READY');
+    setTimeout(function() { robot.demo(demos.Mouse); }, 1000);
+  });
+
+/*
+   robot.on('sensordata', function (data) {
+     console.log('SENSOR DATA', data);
+   });
+ */
+
+  robot.on('bump', function (e) { console.log('BUMP', e); });
+  robot.on('button', function (e) { console.log('BUTTON', e); });
+  robot.on('cliff', function (e) { console.log('CLIFF', e); });
+  robot.on('ir', function (e) { console.log('IR', e); });
+  robot.on('mode', function (e) { console.log('MODE', e); });
+  robot.on('overcurrent', function (e) { console.log('OVERCURRENT', e); });
+  robot.on('virtualwall', function (e) { console.log('VIRTUALWALL', e); });
+  robot.on('wall', function (e) { console.log('WALL', e); });
+  robot.on('wheeldrop', function (e) { console.log('WHEELDROP', e); });
+});
